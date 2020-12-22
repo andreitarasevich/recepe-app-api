8807ff699760a27ea0afdc68ad5f8db6b4a60382
Andrew Tarasevich <andrew.tarasevich@gmail.com>
2020-12-22 15:28:38 +0100

    Fix unit test for test user api. Solve incorrect password check usage.

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,4 +1,4 @@
-from .views import CreateTokenView, CreateUserView
+from .views import CreateTokenView, CreateUserView, ManageUserView
 from django.urls import path
 
 
@@ -7,4 +7,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me')
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 from .serializers import AuthTokenSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions, authentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,3 +15,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for a user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authentificated user"""
+    serializer_class = UserSerialier
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrive and return authentification user"""
+        return self.request.user
