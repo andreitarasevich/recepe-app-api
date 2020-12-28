581cce05097d46a39f1af7a31db82a45ab53d267
Andrew Tarasevich <andrew.tarasevich@gmail.com>
2020-12-28 09:03:35 +0100

    Add create tag funtionality

--- app/recepe/views.py
+++ app/recepe/views.py
@@ -1,12 +1,15 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+# from rest_framework.serializers import Serializer
 
 from core.models import Tag
 from recepe.serializers import TagSerializer
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in database"""
     authentication_classes = {TokenAuthentication, }
     permission_classes = {IsAuthenticated, }
@@ -16,3 +19,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """return objects for authentificated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
