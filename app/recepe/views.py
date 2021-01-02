e0a136dd3576ab95db506b591132ff7c02e8ddc3
Andrew Tarasevich <andrew.tarasevich@gmail.com>
2021-01-02 14:35:49 +0100

    Added create ingredient feature

--- app/recepe/views.py
+++ app/recepe/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework.serializers import Serializer
+
 
 from core.models import Tag, Ingredient
 from recepe.serializers import TagSerializer, IngredientSerializer
@@ -26,7 +26,8 @@ class TagViewSet(viewsets.GenericViewSet,
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage ingredients in the database"""
     authentication_classes = {TokenAuthentication, }
     permission_classes = {IsAuthenticated, }
@@ -36,3 +37,7 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """Return objects for the current authentificated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
