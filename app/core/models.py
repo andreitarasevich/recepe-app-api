6410dd205b344ab422664d39491751d4736088bd
Andrew Tarasevich <andrew.tarasevich@gmail.com>
2021-01-02 14:16:27 +0100

    Add ogredients model

--- app/core/models.py
+++ app/core/models.py
@@ -55,7 +55,7 @@ class Ingredient(models.Model):
     name = CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
--- app/recepe/views.py
+++ app/recepe/views.py
@@ -3,8 +3,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 # from rest_framework.serializers import Serializer
 
-from core.models import Tag
-from recepe.serializers import TagSerializer
+from core.models import Tag, Ingredient
+from recepe.serializers import TagSerializer, IngredientSerializer
 
 
 class TagViewSet(viewsets.GenericViewSet,
@@ -23,3 +23,16 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = {TokenAuthentication, }
+    permission_classes = {IsAuthenticated, }
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authentificated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
